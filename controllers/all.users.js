36e8eca4a666a385c23ad315d0ae95bbbde98fc7
mdismail645221 <mdismail645221@gmail.com>
2023-03-18 00:54:21 +0600

    BONUS: Limit the number of users using query parameter(s) is all done

--- controllers/all.users.js
+++ controllers/all.users.js
@@ -1,8 +1,9 @@
 const allUsers = require('../controllers/random.user')
 
 module.exports.allUsers = (req, res, next) => {
+    const {id} = req.query;
     res.status(200).send({
         success: true,
-        data: allUsers
+        data: allUsers.slice(0, id)
     })
 }
