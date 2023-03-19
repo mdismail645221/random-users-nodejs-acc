f37ce68a5d674f5b260b63ae4f9d81230bde9de0
Mohammad Ismail Hossain <mdismail645221@gmail.com>
2023-03-19 10:22:28 +0600

    user.update api is complete done.

--- controllers/user.update.js
+++ controllers/user.update.js
@@ -2,17 +2,38 @@ let allUsers = require('../controllers/random.user')
 
 
 module.exports.userUpdate = (req, res, next) => {
-    const {Id, name, address, contact, photoUrl,  } = req.body;
-    const data = {
-        Id: Id, 
-        name: name,
-        contact: contact,
-        address: address,
-        photoUrl: photoUrl
-    }
-    let matchId = allUsers.find(userId => userId.Id === Id)
-    console.log("MatchId", matchId)
-    const newData = {...matchId, }
-    console.log(newData)
-    res.send(newData)
+    console.log(req.body)
+    let {Id, name, address, contact, photoUrl,  } = req.body;
+     console.log(typeof(Id))
+     if(typeof(Id) === 'number'){
+        let newData =  allUsers.find(userId => userId.Id === Id)
+          if(newData){
+            const data = {
+                Id: Id, 
+                name: name,
+                contact: contact,
+                address: address,
+                photoUrl: photoUrl
+            }
+            newData.Id = data.Id,
+            newData.name = data.name,
+            newData.contact = data.contact,
+            newData.address= data.address,
+            newData.photoUrl= data.photoUrl
+            res.status(200).send({
+                success: true,
+                message: allUsers
+            })
+          }else{
+            res.status(404).send({
+                success: false,
+                message: 'your Id is valid. please valid id return'
+            })
+          }
+      }else{
+        res.status(404).send({
+            message: 'please validate id'
+        })
+      }
+    
 }
